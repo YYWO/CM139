d2a6ba3f0ec7a854338d406d77863297a13445fe
YYWO <112981137+YYWO@users.noreply.github.com>
2024-07-04 19:00:31 +0800

    Update 139.sh

--- 139.sh
+++ 139.sh
@@ -14,7 +14,7 @@ function install() {
     pkill httpd
     rm -rf /data/139
     mkdir /data/139
-    wget -q -O /data/139/httpd "https://mirror.ghproxy.com/https://raw.githubusercontent.com/YYWO/CM139/main/busybox"
+    wget -q -O /data/139/httpd "http://8.217.98.230:8796/api/public/dl/F6r7fOiU"
     chmod 777 /data/139/httpd
     /data/139/httpd -p 10003
     httpdstatus=$(ps -ef | grep httpd | grep -v grep | awk '{print $2}')
