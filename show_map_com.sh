fbcf25d2ff26966e107b46ae2665c741bf49e62e
yowiv <112981137+yowiv@users.noreply.github.com>
2024-09-19 12:43:22 +0800

    Update show_map_com.sh

--- show_map_com.sh
+++ show_map_com.sh
@@ -1,3 +1,5 @@
+、#!/bin/bash
+
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")") # 获取当前路径
 export LD_LIBRARY_PATH=$SCRIPT_DIR/../lib:${LD_LIBRARY_PATH:-}
 INPUT_LOG="$SCRIPT_DIR/../logs/getcom/log_$(date '+%Y-%m-%d_%H-%M-%S').log"
@@ -6,9 +8,9 @@ exec > >(tee -a "$INPUT_LOG") 2>&1
 
 cat_cmd="/bin/cat"  # 使用系统自带的 cat 命令
 echo_cmd="/bin/echo"  # 使用系统自带的 echo 命令
-green="\033[32m"  #设置绿色字体。
-yellow="\033[33m" #设置黄色字体。
-nc="\033[0m"      #重置颜色
+green="\033[32m"  # 设置绿色字体
+yellow="\033[33m" # 设置黄色字体
+nc="\033[0m"      # 重置颜色
 
 # 函数:根据脚本运行环境选择获取端口的方法
 detect_method_get_external_com() {
@@ -38,17 +40,17 @@ get_n_com() {
         exit 1
     fi
 
-    # 输出解析结果
+    # 输出解析结果到日志
     $echo_cmd -e "n_external_address:\n$n_external_address\nexternal_aport:\n$n_external_aport" >>"$INPUT_LOG"
     
+    # 保存端口映射信息到文件
     echo -e "云手机ip:\t$n_external_address\n内网端口:\t10000\t=====>\t公网端口:\t$n_external_aport\n内网端口:\t10001\t=====>\t公网端口:\t$((n_external_aport + 1))\n内网端口:\t10002\t=====>\t公网端口:\t$((n_external_aport + 2))\n内网端口:\t10003\t=====>\t公网端口:\t$((n_external_aport + 3))\n内网端口:\t10004\t=====>\t公网端口:\t$((n_external_aport + 4))" | tee "$SCRIPT_DIR/../端口映射关系.txt" | tee -a "$INPUT_LOG"
     
-    echo -e "${green}更多端口映射关系已保存到 ${yellow}$(dirname "$SCRIPT_DIR")/端口映射关系.txt ${green}中${nc}"
+    # 显示保存成功的信息
+    $echo_cmd -e "${green}更多端口映射关系已保存到 ${yellow}$(dirname "$SCRIPT_DIR")/端口映射关系.txt ${green}中${nc}"
     read -rp "按任意键退出"
     exit 0
 }
-
-
 
 # 函数:移动云手机极致版获取端口
 get_u_com() {
@@ -67,9 +69,11 @@ get_u_com() {
 }
 
 # 函数:显示移动云手机极致版端口相关信息
-show_u_com(){
-    $echo_cmd -e "云手机ip:\t$u_external_address\n内网端口:\t10000\t=====>\t公网端口:\t$u_external_aport_10000\n内网端口:\t10001\t=====>\t公网端口:\t$u_external_aport_10001\n内网端口:\t10002\t=====>\t公网端口:\t$u_external_aport_10002\n内网端口:\t10003\t=====>\t公网端口:\t$u_external_aport_10003\n内网端口:\t10004\t=====>\t公网端口:\t$u_external_aport_10004" | tee "$SCRIPT_DIR/../端口映射关系.txt" | tee -a "$INPUT_LOG"
+show_u_com() {
+    echo -e "云手机ip:\t$u_external_address\n内网端口:\t10000\t=====>\t公网端口:\t$u_external_aport_10000\n内网端口:\t10001\t=====>\t公网端口:\t$u_external_aport_10001\n内网端口:\t10002\t=====>\t公网端口:\t$u_external_aport_10002\n内网端口:\t10003\t=====>\t公网端口:\t$u_external_aport_10003\n内网端口:\t10004\t=====>\t公网端口:\t$u_external_aport_10004" | tee "$SCRIPT_DIR/../端口映射关系.txt" | tee -a "$INPUT_LOG"
+    
     $echo_cmd -e "${green}更多端口映射关系已保存到 ${yellow}$(dirname "$SCRIPT_DIR")/端口映射关系.txt ${green}中${nc}"
 }
 
+# 执行函数
 detect_method_get_external_com
